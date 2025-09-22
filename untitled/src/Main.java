27856d6b1cd4febed65421a1dff0efa083a0b789
Isaacoun100 <Isaacoun100@estudiantec.cr>
2025-09-22 01:06:09 -0600

    The JFlex now returns the Token

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,11 +1,23 @@
 import java.io.*;
+import java.util.*;
+import obj.token;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // If you pass a file name as argument, use it; otherwise use stdin
         Reader reader = new FileReader("src/test.txt");
-
         absScanner scanner = new absScanner(reader);
-        scanner.yylex(); // this triggers scanning until EOF
+
+        List<token> tokenList = new ArrayList<>();
+        token t;
+
+        // Scan until EOF, adding each token to the list
+        while ((t = scanner.yylex()) != null) {
+            tokenList.add(t);
+        }
+
+        // Print all tokens
+        for (token tok : tokenList) {
+            System.out.println(tok);
+        }
     }
 }
