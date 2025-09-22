e990dcb553a2f6773afc62c7c24cea65b4512294
Alex-MoraKy <167951840+Alex-MoraKy@users.noreply.github.com>
2025-09-22 00:17:06 -0600

    cambio en filename

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         // If you pass a file name as argument, use it; otherwise use stdin
-        Reader reader = new FileReader("src/test.txt");
+        Reader reader = new FileReader("untitled/src/test.txt");
 
         absScanner scanner = new absScanner(reader);
         scanner.yylex(); // this triggers scanning until EOF
